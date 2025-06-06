8fc4ac64bf46788c2faa9b20136ef9d577711175
Enes Esvet Kuzucu <eneskuzucu@hotmail.com>
2025-06-06 17:59:17 +0300

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name='brightdata',  # Package name
-    version='0.1.6',  # Version of your package
+    version='0.1.7',  # Version of your package
     author='Enes Kuzucu',  # Your name
     
     description='Easy to use comprehensive wrapper for brightdata data scraper APIs',  # Short description
