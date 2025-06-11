08f1c015a2e7ce5ec382a66c584066bef4689f55
user.mail <eneskuzucu@hotmail.com>
2025-06-11 16:43:52 +0300

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name='brightdata',  # Package name
-    version='0.2.0',  # Version of your package
+    version='0.2.1',  # Version of your package
     author='Enes Kuzucu',  # Your name
     
     description='Easy to use comprehensive wrapper for brightdata *scrapers, web unlocker, browser) APIs',  # Short description
